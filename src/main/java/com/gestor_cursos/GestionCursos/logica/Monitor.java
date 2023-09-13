903cd9b873970a3d7edf0a0106d2c01e6ae34023
Chopperkasa <limaszandresfelipe@gmail.com>
2023-09-13 18:44:17 -0500

    se elimino la clase funcionario

--- src/main/java/com/gestor_cursos/GestionCursos/logica/Monitor.java
+++ src/main/java/com/gestor_cursos/GestionCursos/logica/Monitor.java
@@ -1,19 +1,73 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.gestor_cursos.GestionCursos.logica;
 
 import java.util.Date;
 
-/**
- *
- * @author sjgar
- */
-public class Monitor extends Funcionario {
+public class Monitor {
     
+    private String codigo;
+    private String nombre;
+    private String telefono;
+    private Date fecha_nacimiento;
+    private String email;
+    private String direccion;
+
     public Monitor(String codigo, String nombre, String telefono, Date fecha_nacimiento, String email, String direccion) {
-        super(codigo, nombre, telefono, fecha_nacimiento, email, direccion);
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.email = email;
+        this.direccion = direccion;
     }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
     
 }
